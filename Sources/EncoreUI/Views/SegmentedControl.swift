3d2323a0b4455d90e111a5fe0d3d7eaa78f6f8c6
Romain <rom4in@me.com>
2022-10-25 11:42:00 +0100

    remove custom value in Segmented control

--- Sources/EncoreUI/Views/SegmentedControl.swift
+++ Sources/EncoreUI/Views/SegmentedControl.swift
@@ -9,7 +9,6 @@ import SwiftUI
 public struct SegmentedControl: View {
     @State var values: [Int]
     @Binding var selectedValue: Int
-    @State private var customValue: Int? = nil
     
     public init(values: [Int], selectedValue: Binding<Int>) {
         self.values = values
@@ -24,17 +23,15 @@ public struct SegmentedControl: View {
                         selectedValue = value
                     }
             }
-            customValueCell
-                .frame(maxWidth: 90)
-            }
-        .background(Color.white.opacity(0.4))
-                .clipShape(Capsule())
-        
+        }
+        .background(Color.transparentWhite)
+        .clipShape(Capsule())
     }
     
     struct ValueCell: View {
         @State var value: Int
         @Binding var selectedValue: Int
+        
         var body: some View {
             Text(value.description)
                 .padding(.horizontal, 20)
@@ -44,32 +41,8 @@ public struct SegmentedControl: View {
                 .background(
                     value == selectedValue ? Color.white : Color.encoreLeastDark)
                 .inter(size: 14)
-                    
+            
         }
-    }
-    
-    private var customValueCell: some View {
-        TextField("Custom", text: Binding(get: {
-            if let value = customValue {
-                return value.description
-            } else {
-                return "Custom"
-            }
-           
-        }, set: { text in
-            if let number = Double(text), number > 0 {
-                customValue = Int(number)
-                selectedValue = customValue ?? 0
-            } else {
-                customValue = nil
-            }
-        }))
-        .keyboardType(.numberPad)
-            .padding(.horizontal, 20)
-            .padding(.vertical, 10)
-            .foregroundColor(customValue == selectedValue ? .black : .gray)
-            .background(customValue == selectedValue ? Color.white : Color.encoreLeastDark)
-            .inter(size: 14)
     }
 }
 
@@ -80,7 +53,6 @@ struct SegmentedControl_Previews: PreviewProvider {
         
         ZStack {
             Color.black
-            
             VStack {
                 Spacer()
                 SegmentedControl(values: [500, 1500, 2000],
