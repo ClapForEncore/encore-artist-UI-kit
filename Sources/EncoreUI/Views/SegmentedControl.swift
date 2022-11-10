0ee525a63d91b7780e01c3e6dc0adc6b1e996f5a
Romain <rom4in@me.com>
2022-11-10 11:57:43 +0000

    update segmented control

--- Sources/EncoreUI/Views/SegmentedControl.swift
+++ Sources/EncoreUI/Views/SegmentedControl.swift
@@ -6,49 +6,49 @@
 
 import SwiftUI
 
+//public struct SegmentedControl: View {
+//    @State var values: [Int]
+//    @Binding var selectedValue: Int
+//
+//    public init(values: [Int], selectedValue: Binding<Int>) {
+//        self.values = values
+//        self._selectedValue = selectedValue
+//    }
+//
+//    public var body: some View {
+//        HStack(spacing: 0.5) {
+//            ForEach(values, id: \.self) { value in
+//                ValueCell(value: value, selectedValue: $selectedValue)
+//                    .onTapGesture {
+//                        selectedValue = value
+//                    }
+//            }
+//        }
+//        .background(Color.transparentWhite)
+//        .clipShape(Capsule())
+//    }
+//
+//    struct ValueCell: View {
+//        @State var value: Int
+//        @Binding var selectedValue: Int
+//
+//        var body: some View {
+//
+//            Text(value.description)
+//                .padding(.horizontal, 20)
+//                .frame(minWidth: 65)
+//                .padding(.vertical, 10)
+//                .foregroundColor(
+//                    value == selectedValue ? .black : .gray)
+//                .background(
+//                    value == selectedValue ? Color.white : Color.black)
+//                .inter(size: 14)
+//        }
+//    }
+//}
+
 public struct SegmentedControl: View {
-    @State var values: [Int]
-    @Binding var selectedValue: Int
     
-    public init(values: [Int], selectedValue: Binding<Int>) {
-        self.values = values
-        self._selectedValue = selectedValue
-    }
-    
-    public var body: some View {
-        HStack(spacing: 0.5) {
-            ForEach(values, id: \.self) { value in
-                ValueCell(value: value, selectedValue: $selectedValue)
-                    .onTapGesture {
-                        selectedValue = value
-                    }
-            }
-        }
-        .background(Color.transparentWhite)
-        .clipShape(Capsule())
-    }
-    
-    struct ValueCell: View {
-        @State var value: Int
-        @Binding var selectedValue: Int
-        
-        var body: some View {
-            
-            Text(value.description)
-                .padding(.horizontal, 20)
-                .frame(minWidth: 65)
-                .padding(.vertical, 10)
-                .foregroundColor(
-                    value == selectedValue ? .black : .gray)
-                .background(
-                    value == selectedValue ? Color.white : Color.black)
-                .inter(size: 14)
-        }
-    }
-}
-
-
-public struct SegmentedTextControl: View {
     @State var values: [String]
     @Binding var selectedValue: String
     @State private var cellWidth: CGFloat = 40
@@ -59,21 +59,22 @@ public struct SegmentedTextControl: View {
     
     public var body: some View {
         
-            GeometryReader { geometry in
-                HStack(spacing: 0.5) {
-                    ForEach(values, id: \.self) { value in
-                        ValueCell(value: value, selectedValue: $selectedValue, width: cellWidth)
-                            .onTapGesture {
-                                selectedValue = value
-                            }
-                    }
-                }
-                .background(Color.transparentWhite)
-                .clipShape(Capsule())
-                .onAppear {
-                    cellWidth = geometry.size.width / CGFloat(values.count)
+        GeometryReader { geometry in
+            HStack(spacing: 0.5) {
+                ForEach(values, id: \.self) { value in
+                    ValueCell(value: value, selectedValue: $selectedValue, width: cellWidth)
+                        .onTapGesture {
+                            selectedValue = value
+                        }
                 }
             }
+            .background(Color.transparentWhite)
+            .clipShape(Capsule())
+            .onAppear {
+                cellWidth = geometry.size.width / CGFloat(values.count)
+            }
+        }.height(36)
+
     }
     
     struct ValueCell: View {
@@ -96,24 +97,24 @@ public struct SegmentedTextControl: View {
 
 
 struct SegmentedControl_Previews: PreviewProvider {
-     @State static var number = 0
+    @State static var number = 0
     @State static var text = "crocodile"
-
     
     static var previews: some View {
         
         ZStack {
             Color.encoreLeastDark
-            VStack {
+            VStack(spacing: 0) {
                 Spacer()
-                SegmentedControl(values: [0, 5, 10, 30],
-                                 selectedValue: $number)
-                SegmentedTextControl(values: ["blue", "red", "sixtyfour", "crocodile"],
-                                 selectedValue: $text)
-                .padding(.horizontal, 30)
-                Spacer()
+                SegmentedControl(values: [0.description, 5.description, 10.description, 30.description],
+                                 selectedValue:
+                                    Binding(get: { number.description },
+                                            set: { newValue in
+                    number = Int(newValue) ?? 0
+                }))
+                SegmentedControl(values:["blue", "red", "sixtyfour", "crocodile"], selectedValue: $text)
             }
-            
         }
     }
+    
 }
