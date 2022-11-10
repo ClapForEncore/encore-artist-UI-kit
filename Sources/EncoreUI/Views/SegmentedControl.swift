a88999f2c3223ec18e4fa98a55d3ec0afc3cdbb4
Romain <rom4in@me.com>
2022-11-10 12:49:47 +0000

    it works!

--- Sources/EncoreUI/Views/SegmentedControl.swift
+++ Sources/EncoreUI/Views/SegmentedControl.swift
@@ -6,47 +6,6 @@
 
 import SwiftUI
 
-//public struct SegmentedControl: View {
-//    @State var values: [Int]
-//    @Binding var selectedValue: Int
-//
-//    public init(values: [Int], selectedValue: Binding<Int>) {
-//        self.values = values
-//        self._selectedValue = selectedValue
-//    }
-//
-//    public var body: some View {
-//        HStack(spacing: 0.5) {
-//            ForEach(values, id: \.self) { value in
-//                ValueCell(value: value, selectedValue: $selectedValue)
-//                    .onTapGesture {
-//                        selectedValue = value
-//                    }
-//            }
-//        }
-//        .background(Color.transparentWhite)
-//        .clipShape(Capsule())
-//    }
-//
-//    struct ValueCell: View {
-//        @State var value: Int
-//        @Binding var selectedValue: Int
-//
-//        var body: some View {
-//
-//            Text(value.description)
-//                .padding(.horizontal, 20)
-//                .frame(minWidth: 65)
-//                .padding(.vertical, 10)
-//                .foregroundColor(
-//                    value == selectedValue ? .black : .gray)
-//                .background(
-//                    value == selectedValue ? Color.white : Color.black)
-//                .inter(size: 14)
-//        }
-//    }
-//}
-
 public struct SegmentedControl: View {
     
     @State var values: [String]
@@ -118,3 +77,4 @@ struct SegmentedControl_Previews: PreviewProvider {
     }
     
 }
+
