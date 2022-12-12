098e9d69ee9e4933c196addf8c75e68d7ca25caf
Romain <rom4in@me.com>
2022-12-12 15:00:30 +0000

    blurview changes

--- Sources/EncoreUI/Views/BackgroundBlurView.swift
+++ Sources/EncoreUI/Views/BackgroundBlurView.swift
@@ -7,16 +7,28 @@
 
 import SwiftUI
 
-public struct BackgroundBlurView: UIViewRepresentable {
+
+public struct BackgroundBlurView: View {
+    
     var darkness = 0.7
-    public init(darkness: CGFloat = 0.7) { }
+    
+    public var body: some View {
+        ZStack {
+            Color.black.opacity(darkness)
+            VisualEffectView()
+        }
+    }
+}
+
+public struct VisualEffectView: UIViewRepresentable {
+    
+    public init() { }
     public func makeUIView(context: Context) -> UIView {
         let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterial))
         DispatchQueue.main.async {
-            view.superview?.superview?.backgroundColor = UIColor.black.withAlphaComponent(darkness)
+            view.superview?.superview?.backgroundColor = .clear
         }
         return view
     }
-    
     public func updateUIView(_ uiView: UIView, context: Context) {}
 }
